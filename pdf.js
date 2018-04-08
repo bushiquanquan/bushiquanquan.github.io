2a45ab535ed5517a6bbb58cb70479b51bb075332
yuanhuihui <yuanhuihui@meituan.com>
2018-04-08 13:39:25 +0800

    t

--- pdf.js
+++ pdf.js
@@ -8262,7 +8262,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var DEFAULT_RANGE_CHUNK_SIZE = 65536;
-var isWorkerDisabled = false;
+// 默认限制Worker
+var isWorkerDisabled = true;
 var workerSrc = void 0;
 var pdfjsFilePath = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : null;
 var fakeWorkerFilesLoader = null;
